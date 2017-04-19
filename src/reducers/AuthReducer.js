2fc92507e2b96b9547e312b9af4611667adb3873
nikfed1 <nikfed1@gmail.com>
2017-04-19 07:46:23 +0300

    new

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,20 @@
+import { List } from 'immutable';
+
+const defaultState = new List();
+
+export default function todoReducer(state = defaultState, action) {
+    switch(action.type) {
+        case 'NEXT_DATE':
+            return state.concat(action);
+        case 'PREVIOUS_DATE':
+            return state.concat(action);
+
+        case 'GET_DEFAULT_DATE':
+            return state.concat(action);
+
+        case 'ADD_RESERVATION':
+            return state.concat(action);
+        default:
+            return state;
+    }
+}
