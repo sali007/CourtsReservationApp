08c21bb480a20921181a55bb689c1060c4fbd349
nikfed1 <nikfed1@gmail.com>
2017-04-22 21:12:19 +0300

    new

--- src/actions/ToDoActions.js
+++ src/actions/ToDoActions.js
@@ -1,8 +1,8 @@
 import request from 'axios';
 
 
-const URL = '/reservation';
-const ADD_RESERVATION_URL = '/addReservation';
+const URL = 'http://127.0.0.1:8080/reservation';
+const ADD_RESERVATION_URL = 'http://127.0.0.1:8080/addReservation';
 
 
 export function nextDate(date) {
