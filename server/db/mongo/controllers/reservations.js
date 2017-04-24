1b8a0991368d5d8998b238c72fc9c452a3e2e2f7
nikfed1 <nikfed1@gmail.com>
2017-04-24 19:28:56 +0300

    new

--- server/db/mongo/controllers/reservations.js
+++ server/db/mongo/controllers/reservations.js
@@ -58,11 +58,11 @@ export function getReservations(req, res) {
 
         res.header('Access-Control-Allow-Methods', 'POST,GET,OPTION')
         res.header('Access-Control-Allow-Origin', '*');
-        res.writeHead(200, {
+        /*res.writeHead(200, {
             'Content-Type': 'text/html;charset=utf-8',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST,GET,OPTIONS'
-        })
+        })*/
 
         console.log('Success in query___getReservations ');
         /*res.header('Accept', 'text/json');
